0087597a8c583b6ce93e817c2d1e6aa302478e5b
nilsreich <nilsreich@gmail.com>
2022-02-16 08:01:51 +0000

    pwa improvements

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { VitePWA } from 'vite-plugin-pwa'
 export default {
   plugins: [
-    VitePWA({})
+    VitePWA({
+      includeAssets: ['favicon-16x16.png', 'favicon.ico'],  
+      manifest: {
+        name: 'Name of your app',
+        short_name: 'Short name of your app',
+        description: 'Description of your app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
   ]    
 }
